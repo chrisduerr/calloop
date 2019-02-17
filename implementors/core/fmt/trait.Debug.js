ec53ec1ea98636b1d328c148c7f2453c680d636f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-17 15:37:32 +0000

    Deploy Smithay/calloop to github.com/Smithay/calloop.git:gh-pages

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["calloop"] = [{text:"impl&lt;E&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"calloop/struct.InsertError.html\" title=\"struct calloop::InsertError\">InsertError</a>&lt;E&gt;",synthetic:false,types:["calloop::loop_logic::InsertError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
